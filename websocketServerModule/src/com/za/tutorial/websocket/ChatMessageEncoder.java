5cc7763a8a3a4c2be30e1dc40c043e8246a6ac23
delf <ttjs2008@naver.com>
2017-04-15 18:17:19 +0900

    first commit

---
+++ websocketServerModule/src/com/za/tutorial/websocket/ChatMessageEncoder.java
@@ -0,0 +1,14 @@
+package com.za.tutorial.websocket;
+import javax.json.Json;
+import javax.websocket.EncodeException;
+import javax.websocket.Encoder;
+import javax.websocket.EndpointConfig;
+public class ChatMessageEncoder implements Encoder.Text<ChatMessage> {
+	public void destroy() {}
+	public void init(EndpointConfig arg0) {}
+	public String encode(ChatMessage message) throws EncodeException {
+		return Json.createObjectBuilder().add("name", message.getName())
+                                         .add("message", message.getMessage())
+                                         .build().toString();
+	}
+}
