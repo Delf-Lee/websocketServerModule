06ef911eb345ea841cf050c4e877292ef73ea7da
delf <ttjs2008@naver.com>
2017-04-15 18:51:07 +0900

    클래스 추가, 메이븐 프로젝트로 변환
     - ChatServerEndpoint.java에 있던 Session set Group으로 이동
     - Group class 추가

--- websocketServerModule/src/com/za/tutorial/websocket/ChatServerEndpoint.java
+++ websocketServerModule/src/com/za/tutorial/websocket/ChatServerEndpoint.java
@@ -1,9 +1,7 @@
 package com.za.tutorial.websocket;
+
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Set;
 
 import javax.websocket.EncodeException;
 import javax.websocket.OnClose;
@@ -12,33 +10,40 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+
 @ServerEndpoint(value = "/chatServerEndpoint", encoders = { ChatMessageEncoder.class }, decoders = { ChatMessageDecoder.class })
 public class ChatServerEndpoint {
-	static Set<Session> chatroomUsers = Collections.synchronizedSet(new HashSet<Session>());
+	private Group group = new Group();
+
 	@OnOpen
 	public void handleOpen(Session userSession) {
-		chatroomUsers.add(userSession);
+		group.getChatroomUsers().add(userSession);
 	}
+
 	@OnMessage
 	public void handleMessage(ChatMessage incomingChatMessage, Session userSession) throws IOException, EncodeException {
 		String username = (String) userSession.getUserProperties().get("username");
 		ChatMessage outgoingChatMessage = new ChatMessage();
-		if (username==null) {
+		if (username == null) {
 			userSession.getUserProperties().put("username", incomingChatMessage.getMessage());
 			outgoingChatMessage.setName("System");
-			outgoingChatMessage.setMessage("you are now connected as "+ incomingChatMessage.getMessage());
+			outgoingChatMessage.setMessage("you are now connected as " + incomingChatMessage.getMessage());
 			userSession.getBasicRemote().sendObject(outgoingChatMessage);
 		} else {
 			outgoingChatMessage.setName(username);
 			outgoingChatMessage.setMessage(incomingChatMessage.getMessage());
-			Iterator<Session> iterator = chatroomUsers.iterator();
-			while (iterator.hasNext()) iterator.next().getBasicRemote().sendObject(outgoingChatMessage);
+			Iterator<Session> iterator = group.getChatroomUsers().iterator();
+			while (iterator.hasNext())
+				iterator.next().getBasicRemote().sendObject(outgoingChatMessage);
 		}
 	}
+
 	@OnClose
 	public void handleClose(Session userSession) {
-		chatroomUsers.remove(userSession);
+		group.getChatroomUsers().remove(userSession);
 	}
+
 	@OnError
-	public void handleError(Throwable t) {}
+	public void handleError(Throwable t) {
+	}
 }
